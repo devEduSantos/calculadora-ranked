e4c6781af7ecb652e655e55953f1e30e8dc7f969
devEduSantos <129607995+devEduSantos@users.noreply.github.com>
2024-07-20 13:23:59 -0300

    PDL RANKEADA CALCULADORA DIO LÓGICA
    20/07/2024

---
+++ main.js
@@ -0,0 +1,22 @@
+function getRank(nickName, victories, defeats){
+    let playerRank;
+    let pdlPlayer = victories - defeats;
+    if(pdlPlayer >= 101){
+        playerRank = "Imortal";
+    }else if(pdlPlayer >= 91){
+        playerRank = "Legendary";
+    }else if(pdlPlayer >= 81){
+        playerRank = "Diamond"
+    }else if(pdlPlayer >= 51){
+        playerRank = "Gold"
+    }else if(pdlPlayer >= 21){
+        playerRank = "Silver"
+    }else if(pdlPlayer >= 11){
+        playerRank = "Bronze";
+    }else{
+        playerRank = "Iron";
+    }
+    return `The hero has ${pdlPlayer} PDL and is in the ${playerRank} Rank`;
+}
+let player1 = getRank("JarvanRei", 10, 30)
+console.log(player1)
